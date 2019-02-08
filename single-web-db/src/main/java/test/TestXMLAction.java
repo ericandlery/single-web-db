c15c9b5eeb587037b31802ac2877258e79d4d75e
Adam <adam@iii.com>
2019-02-08 13:40:00 +0800

    test struts 2 including annotation

---
+++ single-web-db/src/main/java/test/TestXMLAction.java
@@ -0,0 +1,13 @@
+package test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestXMLAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		
+		return super.execute();
+	}
+
+}
---
+++ single-web-db/src/main/java/test/action/TestAction.java
@@ -0,0 +1,13 @@
+package test.action;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/* Notice the Namespace and ResultPath */
+@Namespace("/test")
+@Result(location="test.jsp")
+public class TestAction extends ActionSupport{
+
+}
